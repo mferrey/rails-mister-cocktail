9ef5a76c49453f415b7c4adf5d7d9aeae12cbb3e
mathilde ferrey <mathildeferrey@yahoo.com>
2019-02-14 17:49:32 +0100

    tout OK sauf destroy

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
+  end
+
+private
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "coco")
+
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Vodka Gogo")
+Cocktail.create(name: "Mongo Dingo")
+Cocktail.create(name: "Gin Fizz")
+
+Dose.create(description: "5cl")
+Dose.create(description: "5cl")
+Dose.create(description: "5cl")
+Dose.create(description: "5cl")
